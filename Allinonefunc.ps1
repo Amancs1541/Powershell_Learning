0916e589fcf50ba9ca720f960e4df4fd9de1a8bd
AMAN VARMA <varma1802@outlook.com>
2025-05-19 09:56:49 +0200

    Add files via upload
    The Rest of File

---
+++ Allinonefunc.ps1
@@ -0,0 +1,48 @@
+﻿
+
+Function Get-udfuserinfo{
+
+[cmdletbinding()]
+
+param(
+    $csvfile
+)
+
+return Import-Csv $csvfile
+
+
+}
+
+Function Show-udfUsers{
+
+
+[cmdletbinding()]
+
+param(
+    $userinfo
+
+)
+
+    foreach($user in $userinfo){
+    
+        Write-Output("User : " + $userinfo.Username + "Email is " + $userinfo.Email)
+    
+    }
+
+
+}
+
+#We are going to design another fucntion and that is going to call this two function
+
+Function CallMe(){
+
+    $csvdata=Get-udfuserinfo "C:\Users\Aman Amarjeet Varma\Desktop\Powershell\userinfo.csv"
+
+    Show-udfUsers $csvdata
+
+}
+
+#you can use . <filename> so entire funcTion avaialbe in Memory 
+# you can load entire folder which contain the function 
+
+Get-ChildItem -Path <path> | ForEach-Object{. ${$_.FullName}}
